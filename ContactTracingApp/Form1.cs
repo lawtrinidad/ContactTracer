13ba246b052b454534bf0625f7086d417bca77ff
lawtrinidad <johnlawrence.e.trinidad@gmail.com>
2022-06-06 22:49:16 +0800

    Early Build. UI done. Some functionality.

---
+++ ContactTracingApp/Form1.cs
@@ -0,0 +1,67 @@
+namespace ContactTracingApp
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string FirstName = textBox1.Text;
+            string LastName = textBox2.Text;
+            string Address = textBox3.Text;
+            string PhoneNumber = textBox4.Text;
+            string Email = textBox5.Text;
+            string Time = comboBox1.Text;
+            string Date = (comboBox2.Text + comboBox3.Text + comboBox4.Text);
+            MessageBox.Show(FirstName + LastName + Address + PhoneNumber + Email + Time + Date);
+        }
+    }
+}
