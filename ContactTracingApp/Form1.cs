e18a18d602cdbb94a8200c940eb16dc783f6a85a
lawtrinidad <johnlawrence.e.trinidad@gmail.com>
2022-06-07 22:29:03 +0800

    Text File Creation and Data Recording is working.

--- ContactTracingApp/Form1.cs
+++ ContactTracingApp/Form1.cs
@@ -2,14 +2,12 @@ namespace ContactTracingApp
 {
     public partial class Form1 : Form
     {
-       
+
         public Form1()
         {
             
             InitializeComponent();
             this.DoubleBuffered = true;
-            
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -18,11 +16,6 @@ namespace ContactTracingApp
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -61,6 +54,11 @@ namespace ContactTracingApp
             string Email = textBox5.Text;
             string Time = comboBox1.Text;
             string Date = (comboBox2.Text + comboBox3.Text + comboBox4.Text);
+            StreamWriter recordContact = File.AppendText(@"C:\Users\Public\Documents\Contracter\" + Date + ".txt");
+            recordContact.WriteLine(FirstName + " " + LastName + " " + Address + " " + PhoneNumber + " " + Email + " " + Time + " " + Date);
+            recordContact.Close();
+
+
             MessageBox.Show(FirstName + LastName + Address + PhoneNumber + Email + Time + Date);
         }
     }
