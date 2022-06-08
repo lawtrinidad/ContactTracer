5126d8b66cdfc8bf18aaf5611a7da7ff27214f14
lawtrinidad <johnlawrence.e.trinidad@gmail.com>
2022-06-08 22:54:02 +0800

    Tweaks and polishes.

--- ContactTracingApp/Form1.cs
+++ ContactTracingApp/Form1.cs
@@ -47,6 +47,8 @@ namespace ContactTracingApp
 
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
+            //Declaring the variables
+
             string FirstName = textBox1.Text;
             string LastName = textBox2.Text;
             string Address = textBox3.Text;
@@ -54,12 +56,47 @@ namespace ContactTracingApp
             string Email = textBox5.Text;
             string Time = comboBox1.Text;
             string Date = (comboBox2.Text + comboBox3.Text + comboBox4.Text);
-            StreamWriter recordContact = File.AppendText(@"C:\Users\Public\Documents\Contracter\" + Date + ".txt");
-            recordContact.WriteLine(FirstName + " " + LastName + " " + Address + " " + PhoneNumber + " " + Email + " " + Time + " " + Date);
-            recordContact.Close();
+
+            //To make sure all information is gathered:
+
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == "") || (textBox4.Text == "") || (textBox5.Text == ""))
+            {
+                MessageBox.Show("Please fill up all required fields.");
+                return;
+            }
+
+            if ((comboBox1.Text == "") || (comboBox2.Text == "") || (comboBox3.Text == "") || (comboBox4.Text == ""))
+            {
+                MessageBox.Show("Please include both the date and time of filling up this form.");
+                return;
+            }
+            
+            //Recording the data
+
+             StreamWriter recordContact = File.AppendText(@"C:\Users\Public\Documents\Contracter\" + Date + ".txt");
+             recordContact.WriteLine(FirstName + " " + LastName + " " + Address + " " + PhoneNumber + " " + Email + " " + Time + " ");
+             recordContact.Close();
+
+            //Closing parts
+
+            MessageBox.Show("Thank you for following through with our contact tracing app and protocol.\n\nHave a nice day!");
+
+            foreach (var panel in tableLayoutPanel1.Controls)
+            {
+                foreach (var tools in panel1.Controls)
+                {
+                    if(tools is TextBox)
+                    {
+                        ((TextBox)tools).Text = String.Empty;
+                    }
+                    if (tools is ComboBox)
+                    {
+                        ((ComboBox)tools).SelectedIndex = -1;
+                    }
+                }
+            }
 
 
-            MessageBox.Show(FirstName + LastName + Address + PhoneNumber + Email + Time + Date);
         }
     }
 }
